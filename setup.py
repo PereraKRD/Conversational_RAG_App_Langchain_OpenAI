d91b720b4f38d3a97ef91d28d128df159a282ce1
PereraKRD <ryanperera0531@gmail.com>
2024-09-10 11:16:02 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Letter Template Generator',
+    version='0.0.0',
+    author='Ryan Perera',
+    author_email='ryandell0531@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
+
